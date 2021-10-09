fe0c1b13287f5b3c6c2ca93dbf0de8a963da79f6
Maryam Peer <mary0001@stuent.monash.edu>
2021-10-09 12:17:19 +0400

    Initial commit of map plot

--- js/vegaPlot.js
+++ js/vegaPlot.js
@@ -1,6 +1,4 @@
-var spec2 = "choropleth_symbol_map.vg.json";
-  vegaEmbed('#choropleth_symbol_map', spec2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
-
-console.log("jvnbwjvwhjvb")
+var spec2 = "js/choropleth_symbol_map.vg.json";
+vegaEmbed('#choropleth_symbol_map', spec2, {actions: false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
