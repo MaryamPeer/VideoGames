4349995f3d51c487d2e3d8ec608de60e142ba5f1
Maryam Peer <mary0001@stuent.monash.edu>
2021-10-15 20:12:25 +0400

    Made some changes to the layout

--- js/vegaPlot.js
+++ js/vegaPlot.js
@@ -1,164 +1,30 @@
-var spec1 = "js/choropleth_symbol_map.vl.json";
-vegaEmbed('#choropleth_symbol_map', spec1, {actions: false}).then(function(result) {
+var spec = "js/choropleth_symbol_map.vl.json";
+vegaEmbed('#mapChart', spec, {actions: false}).then(function(result) {
 }).catch(console.error);
 
-var spec2 = "js/radial_plot.vg.json";
-vegaEmbed('#radial_plot', spec2, {actions: false}).then(function(result) {
+spec = "js/radial_plot.vg.json";
+vegaEmbed('#radialChart', spec, {actions: false}).then(function(result) {
 }).catch(console.error);
 
-var spec3 = "js/bar_chart/bar_chart_action.vl.json";
-vegaEmbed('#bar_chart', spec3, {actions: false}).then(function(result) {
+spec = "js/bar_chart/bar_chart_action.vl.json";
+vegaEmbed('#barChart', spec, {actions: false}).then(function(result) {
 }).catch(console.error);
 
-var spec4 = "js/donut_chart/donut_chart_action.vg.json";
-vegaEmbed('#donut_chart', spec4, {actions: false}).then(function(result) {
+spec = "js/donut_chart/donut_chart_action.vg.json";
+vegaEmbed('#donutChart', spec, {actions: false}).then(function(result) {
 }).catch(console.error);
 
 function displayDonutChart(genre){
 
     var spec_bar = "js/bar_chart/bar_chart_" + genre + ".vl.json";
-    vegaEmbed('#bar_chart', spec_bar, {actions: false}).then(function(result) {
+    vegaEmbed('#barChart', spec_bar, {actions: false}).then(function(result) {
     }).catch(console.error);
 
     var spec_donut = "js/donut_chart/donut_chart_" + genre + ".vg.json";
-    vegaEmbed('#donut_chart', spec_donut, {actions: false}).then(function(result) {
+    vegaEmbed('#donutChart', spec_donut, {actions: false}).then(function(result) {
     }).catch(console.error);    
 
     var header = genre.substring(0,1).toUpperCase() + genre.substring(1);
     document.getElementById('selectedGenreDiv1').innerHTML = "<h5>" + header + "</h5>";
     document.getElementById('selectedGenreDiv2').innerHTML = "<h5>" + header + "</h5>";
 }
-
-
-
-
-
-// {
-//     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-//      "params": [
-//           {
-//             "name": "Month_selection",
-//             "value": "January",
-//             "bind": {
-//             "input": "select",
-//             "options": [
-//               "January",
-//               "February",
-//               "March",
-//               "April",
-//               "May",
-//               "June",
-//               "July",
-//               "August",
-//               "September",
-//               "October",
-//               "November",
-//               "December"
-//               ],
-//             "labels":[
-//               "January",
-//               "February",
-//               "March",
-//               "April",
-//               "May",
-//               "June",
-//               "July",
-//               "August",
-//               "September",
-//               "October",
-//               "November",
-//               "December"
-//               ],
-//             "name": "Month "
-//             }
-//           }
-//         ],
-//     "data": {
-//       "url": "https://raw.githubusercontent.com/MaryamPeer/VideoGames/main/data/Twitch_game_data.csv"
-//     },
-//     "transform": [
-//       {"filter": "datum.Month == Month_selection"},
-//       {"filter": "datum.Rank <= 5"}
-//     ],
-//     "hconcat": [
-//       {
-//         "vconcat": [
-//         {
-//           "width": 100,
-//           "height": 100,
-//           "mark": "bar",
-//           "transform": [
-//             {"filter": "datum.Year == 2016"}
-//           ],
-//           "encoding": {
-//             "x": {
-//               "field": "Game",
-//               "type": "nominal",
-//               "sort": {"field": "Hours_watched", "order":"descending"}
-            
-//             },
-//             "y": {"field": "Hours_watched", "type": "quantitative"}
-//           }
-//         },
-//         {
-//           "width": 100,
-//           "height": 100,
-//           "mark": "bar",
-//           "transform": [
-//             {"filter": "datum.Year == 2018"}
-//           ],
-//           "encoding": {
-//             "x": {
-//               "field": "Game",
-//               "type": "nominal",
-//               "sort": {"field": "Hours_watched", "order":"descending"}
-            
-//             },
-//             "y": {"field": "Hours_watched", "type": "quantitative"}
-//           }
-//         }
-//       ]
-//       },
-//       {
-//         "vconcat": [
-//       {
-//         "width": 100,
-//         "height": 100,
-//         "mark": "bar",
-//         "transform": [
-//           {"filter": "datum.Year == 2017"}
-//         ],
-//         "encoding": {
-//           "x": {
-//             "field": "Game",
-//             "type": "nominal",
-//             "sort": {"field": "Hours_watched", "order":"descending"}
-           
-//           },
-//           "y": {"field": "Hours_watched", "type": "quantitative"}
-//         }
-//       },
-//       {
-//         "width": 100,
-//         "height": 100,
-//         "mark": "bar",
-//         "transform": [
-//           {"filter": "datum.Year == 2019"}
-//         ],
-//         "encoding": {
-//           "x": {
-//             "field": "Game",
-//             "type": "nominal",
-//             "sort": {"field": "Hours_watched", "order":"descending"}
-           
-//           },
-//           "y": {"field": "Hours_watched", "type": "quantitative"}
-//         }
-//       }
-//     ]
-//       }
-//     ]
-      
-  
-    
-//   }
